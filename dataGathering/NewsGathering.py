3a8f08892a71bb383c3a9435a2d698b480af8e74
spark638 <spark638@gatech.edu>
2019-03-12 22:05:43 -0400

    added boeing data

--- dataGathering/NewsGathering.py
+++ dataGathering/NewsGathering.py
@@ -3,11 +3,11 @@ import json
 import datetime
 
 company = "boeing"
-fromDate = datetime.datetime.today() - datetime.timedelta(days = 27)
+fromDate = datetime.datetime.today() - datetime.timedelta(days = 27) # past month (past 27 days)
 
 while fromDate <= datetime.datetime.today():
 	page = 1
-	maxPage = 9223372036854775807
+	maxPage = 9223372036854775807 # temporary max page number
 	strFromDate = fromDate.strftime("%Y-%m-%d")
 
 	while page <= maxPage:
@@ -19,6 +19,7 @@ while fromDate <= datetime.datetime.today():
 
 		response = requests.get(url)
 		data = response.json()
+		# update max page number
 		if page == 1:
 			data2 = json.dumps(data)
 			res = json.loads(data2)
@@ -29,10 +30,13 @@ while fromDate <= datetime.datetime.today():
 			if maxPage > 10:
 				maxPage = 10
 
+		# write to json file
 		with open(company + '.json', 'a') as f:
 			json.dump(data['articles'], f)
 			f.write("\n")
 
+		# go to the next page
 		page += 1
 
+	# go to next date
 	fromDate = fromDate + datetime.timedelta(days = 1)
