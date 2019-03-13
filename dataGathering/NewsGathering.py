6d4a29717cbc08074499719e4846f272b76a7f01
spark638 <spark638@gatech.edu>
2019-03-12 22:24:54 -0400

    added news data for more companies

--- dataGathering/NewsGathering.py
+++ dataGathering/NewsGathering.py
@@ -2,7 +2,8 @@ import requests
 import json
 import datetime
 
-company = "boeing"
+# still need to do mcdonald's, cisco, verizon, and fedex
+company = "cisco"
 fromDate = datetime.datetime.today() - datetime.timedelta(days = 27) # past month (past 27 days)
 
 while fromDate <= datetime.datetime.today():
