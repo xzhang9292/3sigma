f2b0a9ab2c49f31b5dd021361b03581bd5f8dbb7
spark638 <spark638@gatech.edu>
2019-04-17 00:41:05 -0400

    added visualization - heatmap

--- dataGathering/getAdjustedCloseOnly.py
+++ dataGathering/getAdjustedCloseOnly.py
@@ -6,7 +6,7 @@ new_data = []
 
 i = 0
 for day in json_data["Time Series (Daily)"]:
-	new_data.append({day: json_data["Time Series (Daily)"][str(day)]["5. adjusted close"]})
+	new_data.append({"Date": day, "Price": json_data["Time Series (Daily)"][str(day)]["5. adjusted close"]})
 	
 with open("boeingAdjustedOnly.json", "w") as outfile:
     json.dump(new_data, outfile)
